5ea1be5f906476a3eb399922bc0f25be940b613b
Victor Dolzan <victorddf5@gmail.com>
2025-03-06 21:12:57 -0300

    Pfx created
    Signed-off-by: Victor Dolzan <victorddf5@gmail.com>

---
+++ AsynchronousProgramming/Pfx.cs
@@ -0,0 +1,49 @@
+﻿namespace AsynchronousProgramming;
+
+public class Pfx
+{
+    public static int InvokeSumParallel()
+    {
+        int sum1 =0, sum2=0, sum3=0;
+        try
+        {
+            Parallel.Invoke(
+                () => sum1 = Sum1(),
+                () => sum2 = Sum2(),
+                () => sum3 = Sum3()
+            );
+        
+            return sum1 + sum2 + sum3;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception thrown: {e.Message}");
+        }
+
+        return -1;
+    }
+    
+    public static int InvokeSumAsync()
+    {
+        return Sum1() + Sum2() + Sum3();
+    }
+
+    private static int Sum1()
+    {
+        //throw new ArgumentException("Exception thrown on Sum1");
+        Thread.Sleep(200);
+        return 20;
+    }
+
+    private static int Sum2()
+    {
+        Thread.Sleep(300);
+        return 20;
+    }
+
+    private static int Sum3()
+    {
+        Thread.Sleep(200);
+        return 20;
+    }
+}
--- AsynchronousProgramming/Program.cs
+++ AsynchronousProgramming/Program.cs
@@ -1,10 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using AsynchronousProgramming;
 
+#region ThreeWaysToStartAThread
+
 // ThreeWaysToStartAThread.RunTask();
+
+#endregion
+
+#region UnderlyingThread
+
 // UnderlyingThread.RunTask();
+
+#endregion
+
+#region Load
+
 // IOTasks.Load();
+
+#endregion
+
+#region TasksRelationship
+
 // TasksRelationship.Nested();
+
+#endregion
+
+#region Cancellation
+
 // Cancellation.Cancel();
-await Combinators.RunTask();
+
+#endregion
+
+#region Combinators
+
+// await Combinators.RunTask();
+
+#endregion
+
+#region Pfx
+
+var totalTime = new Stopwatch();
+totalTime.Start();
+var result = Pfx.InvokeSumParallel();
+totalTime.Stop();
+Console.WriteLine($"Result was {result}. The operation took {totalTime.ElapsedMilliseconds} ms");
+
+#endregion
